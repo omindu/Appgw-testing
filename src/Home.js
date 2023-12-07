2301f128b45b27e4ad914ec36cd2cce834874cf0
omindu <omindu.dishan@gmail.com>
2023-12-07 07:56:58 +0530

    Add logout

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
 
 
 function Home() {
-  const { login, isAuthenticated, user, getToken } = useKindeAuth();
+  const { login, isAuthenticated, user, getToken, logout } = useKindeAuth();
   const [accessToken, setAccessToken] = React.useState(null);
 
   const getAccessToken = async () => {
@@ -13,18 +13,25 @@ function Home() {
 
   return (
     <div>
-      {isAuthenticated ? (
+      {!isAuthenticated ? (
         <div>
+        <h1>Please sign in to continue</h1>
+        {/* Login Button */}
+        <button onClick={() => login()}>Login</button>
+      </div>
+      ) : (
+        <div>
+          {/* Get user info */}
           <h1>Welcome {user.given_name}</h1>
+
+          {/* Get user Access token*/}
           <button onClick={getAccessToken}>Get access token</button>
           <div>
             <pre>{accessToken}</pre>
           </div>
-        </div>
-      ) : (
-        <div>
-          <h1>Please sign in to continue</h1>
-          <button onClick={() => login()}>Login</button>
+
+          {/* Logout Button */}
+          <button onClick={() => logout()}>Logout</button>
         </div>
       )}
     </div>
