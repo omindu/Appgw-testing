ec76f98a5ddfbece4b2f2af0878e3f6a5b7f94ab
omindu <omindu.dishan@gmail.com>
2023-12-22 12:00:52 +0530

    Change Add secure routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom"
+import { RequireSignIn } from "@rownd/react";
+
 import Home from './Home';
+import SecretPage from './SecretPage.js';
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/secret" element={
+        <RequireSignIn>
+          <SecretPage />
+        </RequireSignIn>
+      } />    
     </Routes>
   );
 }
